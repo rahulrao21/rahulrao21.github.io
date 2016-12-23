58a85155f28c59d981cf85c02389edba79c7caa2
rahulrao21 <rahul.21rao@gmail.com>
2016-12-23 17:59:03 +0530

    Update pathvars.js

--- pathvars.js
+++ pathvars.js
@@ -1,1 +1,1 @@
-var pathvars={worker:"https://rahulrao21.github.io/service-worker.js",manifest:"https://rahulrao21.github.io/manifest.json"};
+//var pathvars={worker:"https://rahulrao21.github.io/service-worker.js",manifest:"https://rahulrao21.github.io/manifest.json"};
