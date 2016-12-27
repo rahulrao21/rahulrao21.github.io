8b99bae951beb1b165f93d4cd6bd4f516fa9a594
rahulrao21 <rahul.21rao@gmail.com>
2016-12-27 17:14:32 +0530

    Update servicereq.js

--- servicereq.js
+++ servicereq.js
@@ -1,2 +1,10 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 sleep(2000);
 _pe.subscribe();
