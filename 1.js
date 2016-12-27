3d8f2bc2f4ce9f2ad78397b2b127f0ff0be3f2a3
rahulrao21 <rahul.21rao@gmail.com>
2016-12-27 17:59:20 +0530

    Update and rename 1.html to 1.js

---
+++ 1.js
@@ -0,0 +1,4 @@
+ var pathvars={worker:"/apps/manifestFile/pushengage/service-worker.js",manifest:"/apps/manifestFile/pushengage/manifest.json"};
+ loadjscssfile("https://clientcdn.pushengage.com/core/1.js", "js");
+_pe.subscribe();
+
