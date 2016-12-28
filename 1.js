569af543d422b00508d13fa9d5774f54e8044cf2
rahulrao21 <rahul.21rao@gmail.com>
2016-12-28 15:29:48 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -513,6 +513,7 @@ function attachDialogCss()
         script.src = _peapp.app_subdomain+"/script.js";
         document.getElementsByTagName('head')[0].appendChild(script);
          var _pe={
+          navigator.serviceWorker.register("/apps/manifestFile/pushengage/service-worker.js", {scope: "/"}).then(subscribe);
     subscribe:function(segmentName,callback){
       _pedata.push({"action":"addSubscriberToSegment","data":segmentName});
       var delay=_pe_optin_settings.desktop.optin_delay*1000; 
