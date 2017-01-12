d611a558d1306c6e01576b8a72b4505b52968908
rahulrao21 <rahul.21rao@gmail.com>
2017-01-12 15:06:29 +0530

    Update pathvars.js

--- pathvars.js
+++ pathvars.js
@@ -1,9 +1,9 @@
 var pathvars={worker:"https://mydevstore-15.myshopify.com/apps/manifestFile/service-worker.js",manifest:"https://mydevstore-15.myshopify.com/apps/manifestFile/manifest.json"};
-/*var script = document.createElement('script');
+var script = document.createElement('script');
         script.type = "text/javascript";
         script.src = "https://mydevstore-15.myshopify.com/apps/manifestFile/core/20.js";
         document.getElementsByTagName('head')[0].appendChild(script);
-*/
+
 function subs() {
       if (typeof pe != 'undefined'){
 
