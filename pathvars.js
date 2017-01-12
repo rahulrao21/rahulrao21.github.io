e183bffcef4933033295bb4a1887da7398e7629e
rahulrao21 <rahul.21rao@gmail.com>
2017-01-12 15:01:58 +0530

    Update pathvars.js

--- pathvars.js
+++ pathvars.js
@@ -1,7 +1,16 @@
 var pathvars={worker:"https://mydevstore-15.myshopify.com/apps/manifestFile/service-worker.js",manifest:"https://mydevstore-15.myshopify.com/apps/manifestFile/manifest.json"};
-var script = document.createElement('script');
+/*var script = document.createElement('script');
         script.type = "text/javascript";
         script.src = "https://mydevstore-15.myshopify.com/apps/manifestFile/core/20.js";
         document.getElementsByTagName('head')[0].appendChild(script);
-
-_pe.iframe_subscribe();
+*/
+function subs()
+{
+      if (typeof pe != 'undefined'){
+subs();
+         // pe.subscribe();
+      } else {
+          _pe.subscribe();
+      }
+}
+subs();
