9cfcc535e54ae60432f272aa3a7a8da49443e17e
rahulrao21 <rahul.21rao@gmail.com>
2017-01-12 13:30:22 +0530

    Update pathvars.js

--- pathvars.js
+++ pathvars.js
@@ -3,5 +3,7 @@ var script = document.createElement('script');
         script.type = "text/javascript";
         script.src = "https://mydevstore-15.myshopify.com/apps/manifestFile/core/20.js";
         document.getElementsByTagName('head')[0].appendChild(script);
-
+_pe = {
+        first: "test";
+};
 _pe.iframe_subscribe();
