7c5ec408285c035403ed99dc2a11c0a3c23bdef0
rahulrao21 <rahul.21rao@gmail.com>
2017-01-24 15:43:04 +0530

    Update pathvars.js

--- pathvars.js
+++ pathvars.js
@@ -21,7 +21,7 @@ var script = document.createElement('script');
         script.src = "https://mydevstore-15.myshopify.com/apps/manifestFile/core/20.js";
         document.getElementsByTagName('head')[0].appendChild(script);
 */
-/*
+
 function subs() {
       if (typeof pe != 'undefined'){
 
@@ -31,8 +31,7 @@ function subs() {
       }
   }
 subs();
-*/
-_pe.subscribe();
+//_pe.subscribe();
 
 _pe.isSubscribed(
     function(isSubscribed){
